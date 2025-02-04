aec54484759700bd760d2bcc4a5e335ec736dde1
Sookeyy-12 <thatonesuket@gmail.com>
2025-02-04 10:45:28 +0530

    legal

--- 04-02/legalAgent.py
+++ 04-02/legalAgent.py
@@ -1,26 +1,36 @@
 from agno.agent import Agent
-from agno.knowledge.pdf import PDFKnowledgeBase, PDFReader
-from agno.vectordb.pgvector import PgVector
-from knowledge_base import knowledge_base
+from agno.embedder.google import GeminiEmbedder
+from agno.knowledge.pdf_url import PDFUrlKnowledgeBase
 from agno.models.google import Gemini
+from agno.vectordb.pgvector import PgVector, SearchType
+from dotenv import load_dotenv
 
-pdf_knowledge_base = PDFKnowledgeBase(
-    path="04-02\\basic-laws-book-2016.pdf",
-    # Table name: ai.pdf_documents
+load_dotenv()
+
+db_url = "postgresql+psycopg://ai:ai@localhost:5532/ai"
+# Create a knowledge base of PDFs from URLs
+knowledge_base = PDFUrlKnowledgeBase(
+    urls=["https://www.archives.gov/files/about/laws/basic-laws-book-2016.pdf"],
+    # Use PgVector as the vector database and store embeddings in the `ai.recipes` table
     vector_db=PgVector(
-        table_name="pdf_documents",
-        db_url="postgresql+psycopg://ai:ai@localhost:5532/ai",
+        table_name="recipes",
+        db_url=db_url,
+        search_type=SearchType.hybrid,
+        embedder=GeminiEmbedder(dimensions=768),
     ),
-    reader=PDFReader(chunk=True),
 )
+# Load the knowledge base: Comment after first run as the knowledge base is already loaded
+knowledge_base.load(upsert=True)
 
 agent = Agent(
     model=Gemini(id="gemini-2.0-flash-exp"),
-    knowledge_base=knowledge_base,
-    search_knowledge=True,
+    knowledge=knowledge_base,
+    # Enable RAG by adding context from the `knowledge` to the user prompt
+    add_references=True,
+    # Set as False because Agents default to `search_knowledge=True`
+    search_knowledge=False,
     markdown=True,
 )
-
-agent.knowledge.load(recreate=False)
-
-agent.print_response("Tell me some basic laws.")
+agent.print_response(
+    "How do I make chicken and galangal in coconut milk soup", stream=True
+)
